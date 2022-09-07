8a9108165350e1dc3c04e7445fb28260912e753c
Eddy <63247362+Maestro2496@users.noreply.github.com>
2022-09-07 04:53:20 +0200

    FEEDBACK_DETAILS_ACTIVE

--- components/feedbacks/index.tsx
+++ components/feedbacks/index.tsx
@@ -4,6 +4,7 @@ import Suggestion from "../suggestions/Suggestion";
 import clsx from "clsx";
 
 const Reply = () => {
+  const [showReplyBox, setShowReplyBox] = useState(false);
   return (
     <div className={clsx("flex space-x-6 mt-4 p-2 rounded-md")}>
       <div className="flex flex-col items-center space-y-6 ">
@@ -24,19 +25,36 @@ const Reply = () => {
             <h2 className="text-slate-blue font-bold">Elijah Moss</h2>
             <p className="text-medium-grey">@hexagon.pestagon</p>
           </div>
-          <button className="text-simple-blue font-semibold ">Reply</button>
+          <button
+            onClick={() => setShowReplyBox((prev) => !prev)}
+            className="text-simple-blue font-semibold "
+          >
+            Reply
+          </button>
         </div>
         <div>
           <p className="text-medium-grey">
             Also, please allow styles to be applied based on system preferences. I would love to be
             able to browse Frontend Mentor in the evening after my device’s dark mode turns on
           </p>
+          <div
+            className={clsx(
+              "flex justify-between w-full mt-6 transform transition-all duration-300",
+              showReplyBox ? "opacity-1  translate-y-0" : "opacity-0 h-0 -translate-y-[100%]"
+            )}
+          >
+            <textarea className="w-[80%] h-[5rem] bg-very-light-blue rounded-md focus:outline-none p-4" />
+            <button className="h-1/2 bg-simple-purple text-white py-2 px-3 rounded-md">
+              Post Reply
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 const CommentWithoutReply = () => {
+  const [showReplyBox, setShowReplyBox] = useState(false);
   return (
     <div className="flex space-x-6  px-2 py-6  border-b border-b-gray-300">
       <div className="flex flex-col items-center space-y-6 ">
@@ -57,7 +75,12 @@ const CommentWithoutReply = () => {
             <h2 className="text-slate-blue font-bold">Elijah Moss</h2>
             <p className="text-medium-grey">@hexagon.pestagon</p>
           </div>
-          <button className="text-simple-blue font-semibold ">Reply</button>
+          <button
+            onClick={() => setShowReplyBox((prev) => !prev)}
+            className="text-simple-blue font-semibold "
+          >
+            Reply
+          </button>
         </div>
         <div>
           <p className="text-medium-grey">
@@ -66,12 +89,24 @@ const CommentWithoutReply = () => {
             without the bright background it currently has. Lorem ipsum dolor sit amet consectetur
             adipisicing elit. Amet magnam ipsum officia quo laboriosam aliquid
           </p>
+          <div
+            className={clsx(
+              "justify-between w-full mt-6 transform transition-all duration-300 ",
+              showReplyBox ? "flex" : "hidden"
+            )}
+          >
+            <textarea className="w-[80%] h-[5rem] bg-very-light-blue rounded-md focus:outline-none p-4" />
+            <button className="h-1/2 bg-simple-purple text-white py-2 px-3 rounded-md">
+              Post Reply
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 const CommentWithReply = () => {
+  const [showReplyBox, setShowReplyBox] = useState(false);
   return (
     <div className="flex space-x-6 mt-4  p-2  ">
       <div className="flex flex-col items-center space-y-6 ">
@@ -93,7 +128,12 @@ const CommentWithReply = () => {
             <h2 className="text-slate-blue font-bold">Elijah Moss</h2>
             <p className="text-medium-grey">@hexagon.pestagon</p>
           </div>
-          <button className="text-simple-blue font-semibold ">Reply</button>
+          <button
+            onClick={() => setShowReplyBox((prev) => !prev)}
+            className="text-simple-blue font-semibold "
+          >
+            Reply
+          </button>
         </div>
         <div>
           <p className="text-medium-grey">
@@ -102,6 +142,17 @@ const CommentWithReply = () => {
             without the bright background it currently has. Lorem ipsum dolor sit amet consectetur
             adipisicing elit. Amet magnam ipsum officia quo laboriosam aliquid
           </p>
+          <div
+            className={clsx(
+              "flex justify-between w-full mt-6 transform transition-all duration-300",
+              showReplyBox ? "opacity-1  translate-y-0" : "opacity-0 h-0 -translate-y-[100%]"
+            )}
+          >
+            <textarea className="w-[80%] h-[5rem] bg-very-light-blue rounded-md focus:outline-none p-4" />
+            <button className="h-1/2 bg-simple-purple text-white py-2 px-3 rounded-md">
+              Post Reply
+            </button>
+          </div>
           <Reply />
           <Reply />
         </div>
@@ -142,7 +193,10 @@ export default function FeedBackDetails() {
       <Comments />
       <div className="bg-white px-4 py-6 rounded-md flex flex-col space-y-3 items-center justify-center">
         <h2 className="text-left w-full text-slate-blue text-lg font-bold">Add comment</h2>
-        <textarea className="w-full h-[5rem] bg-very-light-blue rounded-md focus:outline-none p-4" placeholder="Type your comment here"/>
+        <textarea
+          className="w-full h-[5rem] bg-very-light-blue rounded-md focus:outline-none p-4"
+          placeholder="Type your comment here"
+        />
         <div className="w-full flex justify-between items-center">
           <p className="text-medium-grey">250 characters left</p>
           <button className="text-white bg-simple-purple px-4 py-2 rounded-md">Post comment</button>
